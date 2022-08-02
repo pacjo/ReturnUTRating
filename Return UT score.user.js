14acc19dc62769f75aba4729bf3c2d88a4669af1
pacjo <kamil.kroliczek2004@gmail.com>
2022-08-02 17:44:07 +0200

    fixed current rating

--- Return UT score.user.js
+++ Return UT score.user.js
@@ -3,12 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://app.usertesting.com/my_dashboard/available_tests_v3
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      pacjo
 // @description Shows current rating as a number
 // ==/UserScript==
 
-let current_rating =  document.getElementsByClassName("sr-only")[1].innerText.substring(8, 11)
+let current_rating = document.body.textContent.match(/"tester_quality_rating":.+?(?=,)/)[0].substr(document.body.textContent.match("tester_quality_rating")[0].length + 3, 4)
 
 // document.getElementsByClassName("t-ghost mb-0x")[0].innerText = document.getElementsByClassName("t-ghost mb-0x")[0].innerText.substring(1, document.getElementsByClassName("t-ghost mb-0x")[0].innerText = document.getElementsByClassName("t-ghost mb-0x")[0].innerText.length - 1) + ": " + document.getElementsByClassName("sr-only")[1].innerText.substring(8, 11)
 document.getElementsByClassName("t-ghost mb-0x")[0].innerText = document.getElementsByClassName("t-ghost mb-0x")[0].innerText.substring(1, document.getElementsByClassName("t-ghost mb-0x")[0].innerText = document.getElementsByClassName("t-ghost mb-0x")[0].innerText.length - 1) + ": " + current_rating
