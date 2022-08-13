992a019968628c7de35435fa627a5715c3213d49
pacjo <kamil.kroliczek2004@gmail.com>
2022-08-13 10:25:35 +0200

    removed acount id

--- Return UT score.user.js
+++ Return UT score.user.js
@@ -3,40 +3,45 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://app.usertesting.com/my_dashboard/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      pacjo
 // @description Shows current rating and more
 // ==/UserScript==
 
 setTimeout(function () {
 
-    // Calculate values
-    
-    let current_rating = document.body.textContent.match(/"tester_quality_rating":.+?(?=,)/)[0].substring(document.body.textContent.match("tester_quality_rating")[0].length + 3)
-    let account_id = document.body.textContent.match(/"account":{"id":.+?(?=})/)[0].substring(document.body.textContent.match("\"account\":{\"id\":")[0].length)
-    let tests_completed = document.body.textContent.match(/"total_tests_completed":.+?(?=,)/)[0].substring(document.body.textContent.match("total_tests_completed")[0].length + 3)
+  // Calculate values
   
-    
-    // Extended menu
+  console.log("ReturnUTRating started")
   
-    // rating
-    doc = document.createElement("tk-dropdown-footer")
-    doc.innerHTML = "<p class=\"dropdown__link\"></p>"
-    doc.firstChild.innerText = "Current rating: " + current_rating
-    document.getElementsByClassName("mv-2x hydrated")[0].append(doc)
-    
-    // tests completed
-    doc2 = document.createElement("tk-dropdown-item")
-    doc2.innerHTML = "<p class=\"dropdown__link\"></p>"
-    doc2.firstChild.innerText = "Tests completed: " + tests_completed
-    document.getElementsByClassName("mv-2x hydrated")[0].append(doc2)
-    
-    // account id
-    doc3 = document.createElement("tk-dropdown-item")
-    doc3.innerHTML = "<p class=\"dropdown__link\"></p>"
-    doc3.firstChild.innerText = "Account ID: " + account_id
-    document.getElementsByClassName("mv-2x hydrated")[0].append(doc3)
+  let current_rating = document.body.textContent.match(/"tester_quality_rating":.+?(?=,)/)[0].substring(document.body.textContent.match("tester_quality_rating")[0].length + 3)
+  let tests_completed = document.body.textContent.match(/"total_tests_completed":.+?(?=,)/)[0].substring(document.body.textContent.match("total_tests_completed")[0].length + 3)
+  //let account_id = document.body.textContent.match(/"account":{"id":.+?(?=})/)[0].substring(document.body.textContent.match("\"account\":{\"id\":")[0].length)
   
-    console.log("Modification succesful")
-    
-  }, 2000);
+  // Extended menu
+
+  // rating
+  doc = document.createElement("tk-dropdown-footer")
+  doc.innerHTML = "<p class=\"dropdown__link\"></p>"
+  doc.firstChild.innerText = "Current rating: " + current_rating
+  document.getElementsByClassName("mv-2x hydrated")[0].append(doc)
+  
+  // tests completed
+  doc2 = document.createElement("tk-dropdown-item")
+  doc2.innerHTML = "<p class=\"dropdown__link\"></p>"
+  doc2.firstChild.innerText = "Tests completed: " + tests_completed
+  document.getElementsByClassName("mv-2x hydrated")[0].append(doc2)
+  
+  /*
+  
+  // account id
+  doc3 = document.createElement("tk-dropdown-item")
+  doc3.innerHTML = "<p class=\"dropdown__link\"></p>"
+  doc3.firstChild.innerText = "Account ID: " + account_id
+  document.getElementsByClassName("mv-2x hydrated")[0].append(doc3)
+
+  */
+
+  console.log("Modification succesful")
+  
+}, 2000);
